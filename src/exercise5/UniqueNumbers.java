1914717c01fa44ef22c0bcc21949e8084293a3a9
Stilian <stildoge123@abv.bg>
2021-11-11 09:47:10 +0200

    unique

--- src/exercise5/UniqueNumbers.java
+++ src/exercise5/UniqueNumbers.java
@@ -7,18 +7,26 @@ public class UniqueNumbers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter number count: ");
         int numberCount = input.nextInt();
 
         int[] numbers = new int[numberCount];
 
-        System.out.println("Enter numbers: ");
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = input.nextInt();
         }
 
         int[] uniqueNumbers = new int[numbers.length];
-        // Find unique numbers in numbers
+        uniqueNumbers[0] = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (numbers[i] == uniqueNumbers[j]) {
+                    break;
+                }else if(uniqueNumbers[j]==0){
+                    uniqueNumbers[j] = numbers[i];
+                    break;
+                }
+            }
+        }
 
         String uniqueNumbersAsString = Arrays.toString(uniqueNumbers);
         System.out.println("Unique numbers: " + uniqueNumbersAsString);
